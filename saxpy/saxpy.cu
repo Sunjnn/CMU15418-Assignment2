f3255d317dd6d50556acf0e85422c5850f741b1a
sunjianan <sunjianan2021@ia.ac.cn>
2024-03-21 10:57:35 +0800

    feat: finish off the implementation of SAXPY.

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -38,6 +38,9 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     // these buffers are used in the call to saxpy_kernel below
     // without being initialized.
     //
+    cudaMalloc(&device_x, sizeof(float) * N);
+    cudaMalloc(&device_y, sizeof(float) * N);
+    cudaMalloc(&device_result, sizeof(float) * N);
 
     // start timing after allocation of device memory.
     double startTime = CycleTimer::currentSeconds();
@@ -45,10 +48,13 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     //
     // TODO: copy input arrays to the GPU using cudaMemcpy
     //
+    cudaMemcpy(device_x, xarray, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_y, yarray, sizeof(float) * N, cudaMemcpyHostToDevice);
 
     //
     // TODO: insert time here to begin timing only the kernel
     //
+    double startTimeKernel = CycleTimer::currentSeconds();
 
     // run saxpy_kernel on the GPU
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
@@ -60,12 +66,14 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     // ensure the kernel running on the GPU has completed.  (Otherwise
     // you will incorrectly observe that almost no time elapses!)
     //
-    //cudaThreadSynchronize();
+    cudaDeviceSynchronize();
+    double endTimeKernel = CycleTimer::currentSeconds();
 
 
     //
     // TODO: copy result from GPU using cudaMemcpy
     //
+    cudaMemcpy(resultarray, device_result, sizeof(float) * N, cudaMemcpyHostToDevice);
 
     // end timing after result has been copied back into host memory.
     // The time elapsed between startTime and endTime is the total
@@ -80,10 +88,15 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
 
     double overallDuration = endTime - startTime;
     printf("Overall time: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, toBW(totalBytes, overallDuration));
+    overallDuration = endTimeKernel - startTimeKernel;
+    printf("Overall time: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, toBW(totalBytes, overallDuration));
 
     //
     // TODO free memory buffers on the GPU
     //
+    cudaFree(device_x);
+    cudaFree(device_y);
+    cudaFree(device_result);
 }
 
 void
